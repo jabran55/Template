36eda985ccf2afc4dfe3edfe0799f4be8173d3c8
Jabran55 <jabran56@hotmail.com>
2015-05-15 17:06:14 +0100

    create

---
+++ RemoteLoginTest/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace RemoteLoginTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShowPage = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnShowPage
+            // 
+            this.btnShowPage.Location = new System.Drawing.Point(61, 13);
+            this.btnShowPage.Name = "btnShowPage";
+            this.btnShowPage.Size = new System.Drawing.Size(222, 23);
+            this.btnShowPage.TabIndex = 0;
+            this.btnShowPage.Text = "Goto Compare the Market";
+            this.btnShowPage.UseVisualStyleBackColor = true;
+            this.btnShowPage.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(378, 12);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(251, 23);
+            this.btnLogin.TabIndex = 1;
+            this.btnLogin.Text = "Login to Compare the Market";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(38, 69);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1057, 438);
+            this.webBrowser1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(759, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1124, 519);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnShowPage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShowPage;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ RemoteLoginTest/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RemoteLoginTest
+{
+    public partial class Form1 : Form
+    {
+        private bool _IsReports = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://portal.comparethemarket.com/Account/LogOn?ReturnUrl=%2f");
+            webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted);
+        
+
+        }
+
+        void webBrowser1_DocumentCompleted (object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            string s = webBrowser1.DocumentText;
+            btnLogin.Enabled = true;
+            Thread.Sleep(2000);
+
+            if(_IsReports)
+            {
+             HtmlElement ele = webBrowser1.Document.GetElementById("ReportViewer1_ctl04_ctl11_ddValue");
+                if (ele != null)
+                    ele.SetAttribute("Value", "1");
+
+                ele = webBrowser1.Document.GetElementById("ReportViewer1_ctl04_ctl13_ddValue");
+                if (ele != null)
+                    ele.SetAttribute("Value", "3");
+                ele = webBrowser1.Document.GetElementById("ReportViewer1_ctl04_ctl05_ddValue");
+                if (ele != null)
+                    ele.SetAttribute("Value", "1");
+
+
+                ele = webBrowser1.Document.GetElementById("ReportViewer1_ctl04_ctl07_txtValue");
+                if (ele != null)
+                {
+                    ele.InnerText = "01/04/15";
+                }
+
+
+                ele = webBrowser1.Document.GetElementById("ReportViewer1_ctl04_ctl09_txtValue");
+                if (ele != null)
+                {
+                    ele.InnerText = "01/04/15";
+                }
+
+
+                ele = webBrowser1.Document.GetElementById("ReportViewer1_ctl04_ctl00");
+
+                if (ele != null)
+                {
+                    ele.InvokeMember("click");
+                }
+                else
+                {
+                    MessageBox.Show("Button not found");
+                }
+
+                _IsReports = false;
+            }
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnLogin.Enabled = false;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+            AutoResetEvent _resetevent = new AutoResetEvent(false);
+
+            HtmlElement ele = webBrowser1.Document.GetElementById("UserName");
+            if (ele != null)
+                ele.InnerText = "CTM_THA01";
+            ele = webBrowser1.Document.GetElementById("Password");
+            if (ele != null)
+                ele.InnerText = "daxa7Ast";
+            ele = webBrowser1.Document.GetElementById("ButtonAuthenticate");
+            if (ele != null)
+            {
+                ele.InvokeMember("click");
+                _IsReports = true;
+                Thread.Sleep(1000);
+                webBrowser1.Navigate("https://portal.comparethemarket.com/Home/Reports");
+                webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted);
+               
+            }
+           // EventWaitHandle.WaitAll(new AutoResetEvent[] { _resetevent });
+
+          
+                   
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
